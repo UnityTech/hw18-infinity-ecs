2fd65fecb603a86506a9686ad37606de04dd61b0
Aleksandr Kirillov <aleksandrk@unity3d.com>
2018-06-26 15:07:40 +0200

    Fixed bootstrap

--- Assets/Scripts/Miscellaneous/Bootstrap.cs
+++ Assets/Scripts/Miscellaneous/Bootstrap.cs
@@ -6,16 +6,9 @@ namespace Unity.InfiniteWorld
 {
     public class Bootstrap
     {
-        public static EntityArchetype terrainArchetype;
-
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         public static void Initialize()
         {
-            var entityManager = World.Active.GetOrCreateManager<EntityManager>();
-            World.Active.GetOrCreateManager<TerrainChunkAssetDataSystem>(); // Instantiate only
-            World.Active.GetOrCreateManager<TerrainChunkRenderSystem>();
-
-            terrainArchetype = entityManager.CreateArchetype(typeof(Sector), typeof(LOD), typeof(Transform));
         }
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
@@ -23,13 +16,6 @@ namespace Unity.InfiniteWorld
         {
             World.Active.GetOrCreateManager<CameraControlSystem>().Init(Camera.main);
             World.Active.GetOrCreateManager<TerrainChunkLifecycleSystem>().Init(Camera.main);
-
-            var entityManager = World.Active.GetOrCreateManager<EntityManager>();
-            var entity = entityManager.CreateEntity(terrainArchetype);
-            entityManager.SetComponentData<Sector>(entity, new Sector() { value = new int2(0, 0) });
-
-            var entity2 = entityManager.CreateEntity(terrainArchetype);
-            entityManager.SetComponentData<Sector>(entity2, new Sector() { value = new int2(1, 0) });
         }
     }
 }
