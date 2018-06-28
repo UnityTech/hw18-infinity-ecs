ac851de41f29cfa9ed431394a15ca090fe0c7b9e
Anton <antonk@unity3d.com>
2018-06-28 12:24:33 +0200

    Debug output

--- Assets/Scripts/DebugOutput.cs
+++ Assets/Scripts/DebugOutput.cs
@@ -21,15 +21,15 @@ namespace Unity.InfiniteWorld
 
         protected override void OnUpdate()
         {
-            /*
             var count = EntityManager.Debug.EntityCount;
-            var transform = EntityManager.GetComponentData<Transform>(camera.main);
+            var shift = EntityManager.GetComponentData<Shift>(camera.main);
+            var sector = EntityManager.GetComponentData<Sector>(camera.main);
+            float3 truePos = new float3(sector.value.x * WorldChunkConstants.ChunkSize, 0, sector.value.y * WorldChunkConstants.ChunkSize) + shift.value;
 
             StringBuilder builder = new StringBuilder();
-            builder.AppendFormat("ENTITIES {0} \n COORDINATES: {1}:{2}:{3}", count, transform.transform.c3.x, transform.transform.c3.y, transform.transform.c3.z);
+            builder.AppendFormat("ENTITIES {0:D6} \n COORDINATES: {1:F1}:{2:F1}:{3:F1}", count, truePos.x, truePos.y, truePos.z);
 
             debugText.text = builder.ToString();
-            */
         }
     }
 }
